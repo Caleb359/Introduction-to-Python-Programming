4423030a92ab7c5e81ffde3f97a4209ee7ccd792
Caleb359 <nyamaricaleb6@gmail.com>
2025-02-06 18:35:25 +0300

    User Input , IF & Elif statements and data types

--- First.py
+++ First.py
@@ -1,5 +1,41 @@
-Ngoingwa = "Where I live"
-WebDesign = "What I study"
-Carwash = "Wash Cars"
-print(Carwash)
+Carwash = "wash cars"
+Webdevt = "working on"
+Ngoingwa = "homestead"
+#print(Carwash)
+Age =25
+Height = 5.7
+Its_student = True
+Over_18 = False
+#print(Its_student)
+Reams = 2
+Points = 83.9
+Uniform = False
+Electronics = True
+#print(Points, Reams, Uniform, Electronics)
+x = 10
+y = 5
+print(x+y) #addition
+print(x-y) #subtraction
+print(x/y) #division
+print(x*y) #multiplication
+print(x**y) #power
+print(x%y) #modulus
+#Your_age = int(input ("Enter your age\n"))
+#Your_height = float(input ("Enter your height\n"))
 
+# Learning if statements
+#if (Your_age) < 18 : print("You are a minor")
+#else: print("You are an adult")
+'''CAT = int(input ("enter your cat marks\n"))
+EXAM = int(input ("enter your exam marks\n"))
+Total_marks = CAT + EXAM
+print("Total marks:")
+print(Total_marks)
+if Total_marks < 40: print("Grade F\n")
+elif (Total_marks) >= 40 and (Total_marks)< 50: print("Grade D\n")
+elif (Total_marks) >= 50 and (Total_marks)< 60: print("Grade C\n")
+elif (Total_marks) >= 60 and (Total_marks)< 70: print("Grade B\n")
+else: print("Grade A")'''
+
+Username = input ("Enter your username/n")
+print("your username: " + Username)
