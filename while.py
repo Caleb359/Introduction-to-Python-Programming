65674cc42c7cf07b81ece236a1471360c532e636
Caleb359 <nyamaricaleb6@gmail.com>
2025-02-07 18:20:32 +0300

    do while loop

--- while.py
+++ while.py
@@ -3,7 +3,39 @@ while i<5:
     print(i)
     i += 1'''
 
-i=50
+'''i=50
 while i>=25:
         print(i)
-        i -= 1
+        i -= 1'''
+
+        #Do while loop
+'''i = 0
+while True:
+    print(i)
+    i += 1
+    if i > 5:
+        break'''
+
+i = 40
+while True:
+    print(i)
+    i -= 1
+    if i < 20:
+        break
+
+i = 3
+while True:
+    print(i)
+    i += 1
+    if i > 9:
+        break
+
+i = 15
+while True:
+    print(i)
+    i -= 3
+    if i < 2:
+        break
+        
+
+
