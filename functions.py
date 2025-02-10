dcd7eb8884f6c0c14d2dac9494b3ef3409f47dc7
Caleb359 <nyamaricaleb6@gmail.com>
2025-02-10 17:54:22 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,41 @@
+'''def students(names, name):
+        return names, name
+print(students("Ian", "Josh"))'''
+
+'''def phones(name):
+        return name
+print(phones("infinix"))'''
+        #sum function that takes userr inputs and returns sum of two numbers
+'''def sum(x, y):
+      x = float(input("enter the first number\n"))
+      y = float(input("enter the second number\n"))
+      result = x+y
+      print(result)
+      return result
+    
+sum()    '''
+
+            #function that takes the sum two numbers
+'''def sum(x, y):
+    result = x+y
+    print(result)
+    return result
+
+sum(10,20)'''
+           
+            #function that takes the multiplication of two numbers
+
+def multiplication():  
+    x = float(input("enter the first number \n"))    
+    y = float(input("enter the second number\n"))
+    result = x*y 
+    print(result)
+    return result
+multiplication()
+
+
+      
+
+
+      
+        
